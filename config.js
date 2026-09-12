575ca076bae62b5ba058e714d6b0166963ddee65
Piper <readuvall@gmail.com>
2026-09-12 19:12:55 -0400

    Delete config.js

--- config.js
+++
@@ -1,7 +0,0 @@
-// Public configuration only. Never place API keys in this file.
-window.MLW_CONFIG = {
-  subscriptionApiUrl: "",
-  representativeFinderUrl: "https://mgaleg.maryland.gov/mgawebsite/Members/District",
-  myMgaUrl: "https://mgaleg.maryland.gov/mgawebsite/MyMGATracking/WitnessSignup"
-};
-
