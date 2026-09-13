3fb12c1a652a82eca78be4f53ce3786e61bd4d2b
Piper <readuvall@gmail.com>
2026-09-12 21:34:02 -0400

    Update config.js

--- site/config.js
+++ site/config.js
@@ -1,6 +1,6 @@
 // Public configuration only. Never place API keys in this file.
 window.MLW_CONFIG = {
-  subscriptionApiUrl: "",
+  subscriptionApiUrl: "https://maryland-legislative-watch-subscriptions.readuvall.workers.dev",
   representativeFinderUrl: "https://mgaleg.maryland.gov/mgawebsite/Members/District",
   myMgaUrl: "https://mgaleg.maryland.gov/mgawebsite/MyMGATracking/WitnessSignup"
 };
